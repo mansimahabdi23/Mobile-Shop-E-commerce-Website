b01296fb3d5ee5379ec7107bbb1c19902a879371
Aaru <{Workingaaru@gmail.com}+{Aaru}@users.noreply.github.com>
2024-03-15 10:24:21 +0530

    v0.1 (update AdminRoute)

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -83,7 +83,7 @@ const Header = () => {
   </NavLink>
   <ul className="dropdown-menu">
     <li>
-      <NavLink to = {`/dashboard/${
+      <NavLink to = {`dashboard/${
       auth?.user?.role === 1 ? "admin" : "user"
        }`}
          className="dropdown-item"
